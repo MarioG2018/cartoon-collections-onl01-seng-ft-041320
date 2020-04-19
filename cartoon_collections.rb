b8a34da397d0f64322c706a88d7f59340935f959
Mario Garza <mario.garza02@utrgv.edu>
2020-04-19 02:35:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,10 +20,10 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese(food)
+def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.find do |cheese|
+  foods.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
