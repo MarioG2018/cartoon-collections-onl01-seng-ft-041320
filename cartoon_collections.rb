ba39809f290e879c09e45713674d4f4393456587
Mario Garza <mario.garza02@utrgv.edu>
2020-04-19 01:48:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,6 @@ def summon_captain_planet(calls)
   call.capitalize+"!"
   end
 end
-#def summon_captain_planet(planeteer_calls)
-#  planeteer_calls.map { |call| call.capitalize + "!" }
-#end
 
 def long_planeteer_calls(calls)
   calls.any? do|call|
