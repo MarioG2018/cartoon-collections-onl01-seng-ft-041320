56ced96625851d11b87f8681038cc7c89b4aa58b
Mario Garza <mario.garza02@utrgv.edu>
2020-04-19 02:25:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   #cheese_types.any? do |cheese|
-  if food.include?(cheese_types)
+  if food.find?(cheese_types)
     cheese_types
   else
     puts "NO"
